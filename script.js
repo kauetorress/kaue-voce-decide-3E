e93267cc785ff774ec1d74e2574145d687d70346
kauetorress <165967827+kauetorress@users.noreply.github.com>
2024-05-29 23:47:19 +0000

    29/05/24

---
+++ script.js
@@ -0,0 +1,47 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-principal");
+const caixaAlternativas = document.querySelector(".caixa-principal");
+const caixaResultado = document.querySelector(".caixa-principal");
+const textoResultado = document.querySelector(".caixa-principal");
+
+const perguntas = [
+  {
+    enunciado:"Pergunta 1",
+    alternativas:[
+      "Resposta 1"
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enunciado:"Pergunta 2",
+    alternativas:[
+      "Resposta 1"
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enunciado:"Pergunta 3",
+    alternativas:[
+      "Resposta 1"
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enunciado:"Pergunta 4",
+    alternativas:[
+      "Resposta 1"
+      "Resposta 2"
+    ]
+  },
+
+  {
+    enunciado:"Pergunta 5",
+    alternativas:[
+      "Resposta 1"
+      "Resposta 2"
+    ]
+  }
+];
